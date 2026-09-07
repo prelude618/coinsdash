27ad4f8aa977e8fd73c1a6a585982f797a219405
David K Lee <dlee@nametag.co>
2026-09-07 11:50:56 -0700

    Bump app version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "com.holyware.coinsdash"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
